2294c1ffb61e0b540532f250acd69a5d3eb72871
arhamabeer <arhamabeerahmed@gmail.com>
2023-05-04 00:10:43 +0500

    3-5/home

---
+++ Migrations/20230503170028_IdentityFramework.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnet_mvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class IdentityFramework : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

c2d91aec89a0554ff3b7d2086d1e688bc4806197
Arham Abeer Ahmed <arham.abeer@braincrop.net>
2023-05-10 17:38:01 +0500

    10-5/ofc

---
+++ Controllers/AdministratorController.cs
@@ -0,0 +1,45 @@
+﻿namespace dotnet_mvc.Controllers
+{
+    public class AdministratorController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public AdministratorController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+
+        [HttpGet] 
+        public IActionResult CreateRole() {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var identityRole = new IdentityRole
+                {
+                    Name = model.role
+                };
+                var result = await roleManager.CreateAsync(identityRole);
+                Console.WriteLine("AA" + result.Succeeded);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("GetRoles", "Administrator");
+                }
+                foreach (var err in result.Errors)
+                {
+                    ModelState.AddModelError("", err.Description);
+                }
+            }
+            return View(model);
+        }
+
+        public IActionResult GetRoles()
+        {
+            var roles = roleManager.Roles;
+            return View(roles);
+        }
+    }
+}
---
+++ ViewModels/CreateRoleViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace dotnet_mvc.ViewModels
+{
+    public class CreateRoleViewModel
+    {
+        [Required]
+        [Display(Name = "Role Name")]
+        public string role { get; set; }
+    }
+}

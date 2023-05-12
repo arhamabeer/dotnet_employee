99940945a5fad2f3e278f49d83d459d3bbcd6ae7
Arham Abeer Ahmed <arham.abeer@braincrop.net>
2023-05-12 17:44:27 +0500

    12-5/ofc

--- Controllers/AdministratorController.cs
+++ Controllers/AdministratorController.cs
@@ -4,9 +4,12 @@
     {
         private readonly RoleManager<IdentityRole> roleManager;
 
-        public AdministratorController(RoleManager<IdentityRole> roleManager)
+        public UserManager<ApplicationUser> UserManager;
+
+        public AdministratorController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
         {
             this.roleManager = roleManager;
+            this.UserManager = userManager;
         }
 
         [HttpGet] 
@@ -40,6 +43,62 @@
         {
             var roles = roleManager.Roles;
             return View(roles);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string id)
+        {
+            var role = await roleManager.FindByIdAsync(id);
+            if(role == null)
+            {
+                ViewBag.Msg = $"No Role found of ID: {id}";
+                return View("NotFound");
+
+            }
+            else
+            {
+                var model = new EditRoleViewModel
+                {
+                    id = id,
+                    role = role.Name
+                };
+                foreach(var user in UserManager.Users)
+                {
+                    if(await UserManager.IsInRoleAsync(user, role.Name))
+                    {
+                        model.users.Add(user);
+                    }
+                }
+                return View(model);
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleViewModel model)
+        {
+            var role = await roleManager.FindByIdAsync(model.id);
+            if(role == null)
+            {
+                ViewBag.Msg = $"No Role found of ID: {model.id}";
+                return View("NotFound");
+
+            }
+            else
+            {
+                role.Name = model.role; 
+                var result = await roleManager.UpdateAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("GetRoles", "Administrator");
+                }
+                else
+                {
+                    foreach(var err in  result.Errors)
+                    {
+                        ModelState.AddModelError("", err.Description);
+                    }
+                    return View(model);
+                }
+            }
         }
     }
 }
---
+++ ViewModels/EditRoleViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace dotnet_mvc.ViewModels
+{
+    public class EditRoleViewModel
+    {
+        public EditRoleViewModel()
+        {
+            users = new List<ApplicationUser>();
+        }
+        public string id { get; set; }
+        [Required]
+        [Display(Name = "Role Name")]
+        public string role { get; set; }
+        public List<ApplicationUser> users { get; set; }
+    }
+}

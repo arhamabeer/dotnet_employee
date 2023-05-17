40c79b7fa39e59f458f1aec29841c89720b913a5
Arham Abeer Ahmed <arham.abeer@braincrop.net>
2023-05-17 17:49:01 +0500

    ofc/17-5-23

--- Controllers/AdministratorController.cs
+++ Controllers/AdministratorController.cs
@@ -1,4 +1,6 @@
-﻿namespace dotnet_mvc.Controllers
+﻿using System.Data;
+
+namespace dotnet_mvc.Controllers
 {
     public class AdministratorController : Controller
     {
@@ -100,5 +102,90 @@
                 }
             }
         }
+
+        [HttpGet]
+        public async Task<IActionResult> EditUserInRole(string id)
+        {
+            ViewBag.role = id;
+
+            var role = await roleManager.FindByIdAsync(id);
+            if(role == null)
+            {
+                ViewBag.Msg = $"Role with ID: {id} is not found...";
+                return RedirectToAction("NotFound");
+            }
+            var model = new List<EditUserInRoleViewModel>();
+
+            foreach(var user in UserManager.Users)
+            {
+                var userModel = new EditUserInRoleViewModel
+                {
+                    UserId = user.Id,
+                    userName = user.UserName
+                };
+
+                if(await UserManager.IsInRoleAsync(user, role.Name)){
+                    userModel.isSelected = true;
+                }
+                else
+                {
+                    userModel.isSelected = false;
+                }
+                model.Add(userModel);
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditUserInRole(List<EditUserInRoleViewModel> model, string id)
+        {
+            var role = await roleManager.FindByIdAsync(id);
+            if (role == null)
+            {
+                ViewBag.Msg = $"Role with ID: {id} is not found...";
+                return RedirectToAction("NotFound");
+            }
+
+            for (var i = 0; i < model.Count; i++)
+            {
+                var user = await UserManager.FindByIdAsync(model[i].UserId);
+                IdentityResult result = null;
+                
+                if (model[i].isSelected && !(await UserManager.IsInRoleAsync(user, role.Name)))
+                {
+                    result = await UserManager.AddToRoleAsync(user, role.Name);
+                }
+                else if (!model[i].isSelected && await UserManager.IsInRoleAsync(user, role.Name))
+                {
+                    result  = await UserManager.RemoveFromRoleAsync(user, role.Name);
+                }
+                else
+                {
+                    continue;
+                }
+                if (result.Succeeded)
+                {
+                    if(i == (model.Count - 1))
+                    {
+                        return RedirectToAction("EditRole", new { id = id });
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else
+                {
+                    foreach(var err in result.Errors)
+                    {
+
+                        ModelState.AddModelError("", err.Description);
+                    }
+                }
+            }
+            return RedirectToAction("EditRole", new { id = id});
+
+        }
+
     }
 }
---
+++ ViewModels/EditUserInRoleViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace dotnet_mvc.ViewModels
+{
+    public class EditUserInRoleViewModel
+    {
+        public string UserId { get; set; }
+        public string userName { get; set; }
+        public bool isSelected { get; set; }
+    }
+}
